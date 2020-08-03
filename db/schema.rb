37f2ccedfd1f9919b9b281b05c0fd5e26a007678
Abhay <ch.abhay@yahoo.com>
2020-08-03 20:23:29 +0530

    Finish Toy App

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_02_094959) do
+ActiveRecord::Schema.define(version: 2020_08_03_130301) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
